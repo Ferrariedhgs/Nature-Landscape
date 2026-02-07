2ecce19708d77078a44c319501c076082c477a47
Buia Alexandru <45429773+Ferrariedhgs@users.noreply.github.com>
2026-02-07 15:22:25 +0200

    Refactor image generation and saving process
    Updated image saving logic and added timing for batch processing.

--- nature_landscape/run.py
+++ nature_landscape/run.py
@@ -2,12 +2,12 @@ from diffusers import StableDiffusionXLPipeline
 import torch
 import gc
 import os
+import time
 
-# ---- config ----
 out_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"output")
 
-p_prompt = "realistic nature landscape" # Your prompt here
-n_prompt = "ugly, blurry, poor quality, text, watermark" # Negative prompt here
+p_prompt = "realistic nature landscape"
+n_prompt = "ugly, blurry, poor quality, text, watermark"
 
 os.makedirs(out_dir, exist_ok=True)
 
@@ -18,22 +18,24 @@ meta = {
     "seed": "i"
 }
 
-
 pipe = StableDiffusionXLPipeline.from_pretrained("segmind/SSD-1B", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.to("cuda")
 #pipe.enable_attention_slicing()
 
+batch_size=1024
 
-
-for i in range(0,10):
+start=time.time()
+for i in range(batch_size):
     gen = torch.Generator("cuda").manual_seed(i)
     image = pipe(prompt=p_prompt, negative_prompt=n_prompt, generator=gen, height=512, width=512).images[0]
 
-    out_path = os.path.join(out_dir, f'image_00{i}.png')
+    out_path = os.path.join(out_dir, f'image_{i}.png')
     image.save(out_path)
+    print(i)
 
-    print(f"Saved 10 images")
-
+    #free some vram for low vram gpus
     del image
     gc.collect()
     torch.cuda.empty_cache()
+
+print(f'Saved {batch_size} images, time: {time.time()-start} s')
